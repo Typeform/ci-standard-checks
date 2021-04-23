1825b4184d7095993d2ca42b7a5ad062911714a0
Kao Félix <kao.felix@typeform.com>
2021-04-23 10:14:11 +0200

    feat(SETI-957): run gitleaks check using @actions/exec

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
+import * as path from 'path'
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as exec from '@actions/exec'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const dockerUsername: string = core.getInput('docker-username')
+    const dockerPassword: string = core.getInput('docker-password')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const checkPath = path.join(__dirname, '..', 'checks', 'secrets-scan.sh')
 
-    core.setOutput('time', new Date().toTimeString())
+    exec.exec('bash', [checkPath], {
+      env: {
+        PATH: process.env['PATH'] || '',
+        DOCKER_PASSWORD: dockerPassword,
+        DOCKER_USERNAME: dockerUsername
+      }
+    })
   } catch (error) {
     core.setFailed(error.message)
   }
