cf3cc6a77624e7a2472dc91d0394ea07ea723cb1
Ezequiel Russo <erusso87@gmail.com>
2021-09-08 16:26:28 +0200

    chore(BEAR-2701): try to pull directly from ECR

--- scripts/validate-openapi/run.sh
+++ scripts/validate-openapi/run.sh
@@ -11,16 +11,12 @@ then
 fi
 
 DOCKER_REGISTRY="567716553783.dkr.ecr.us-east-1.amazonaws.com"
-DOCKER_IMAGE="engineering-docs"
-DOCKER_IMAGE_TAG="builder"
+DOCKER_IMAGE="engineering-docs:builder"
 
 # Check if user is logged in to DOCKER_REGISTRY
-docker login -u=${DOCKERUSERNAME} -p=${DOCKERPASSWORD} ${DOCKER_REGISTRY}
-docker pull ${DOCKER_REGISTRY}/${DOCKER_IMAGE}:${DOCKER_IMAGE_TAG}
-exit_code=$?
-
-if [ ! $exit_code -eq 0 ]; then
-    echo "Unable to pull ${DOCKER_IMAGE}:${DOCKER_IMAGE_TAG} image. Are you logged in ${DOCKER_REGISTRY}?"
+docker pull ${DOCKER_REGISTRY}/${DOCKER_IMAGE}
+if [ ! $? -eq 0 ]; then
+    echo "Unable to pull ${DOCKER_IMAGE} image. Are you logged in ${DOCKER_REGISTRY}?"
     exit 1
 fi
 
@@ -31,7 +27,7 @@ fi
 
 docker run --rm \
 	-v $(pwd)/openapi.yaml:/app/openapi.yaml \
-	${DOCKER_REGISTRY}/${DOCKER_IMAGE}:${DOCKER_IMAGE_TAG} \
+	${DOCKER_REGISTRY}/${DOCKER_IMAGE} \
 	sh -c \
         "  openapi bundle --dereferenced /app/openapi.yaml > /app/openapi.der.yaml \
 	        && swagger-cli validate /app/openapi.yaml \
