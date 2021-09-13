bac3cbbe559191ef50a6bd99ab70dd0107b2930a
Ezequiel Russo <erusso87@gmail.com>
2021-09-13 15:49:19 +0200

    chore(BEAER-2701): Install npm dependencies to avoid docker registry

--- scripts/validate-openapi/run.sh
+++ scripts/validate-openapi/run.sh
@@ -3,34 +3,23 @@
 # exit when any command fails
 set -e
 
-# Check if docker is installed
-if ! command -v "docker" &> /dev/null
-then
-    echo "Unable to find docker. Is it installed and added to your \$PATH?"
-    exit 1
-fi
-
-DOCKER_REGISTRY="567716553783.dkr.ecr.us-east-1.amazonaws.com"
-DOCKER_IMAGE="engineering-docs:builder"
-
-# Check if user is logged in to DOCKER_REGISTRY
-docker pull ${DOCKER_REGISTRY}/${DOCKER_IMAGE}
-if [ ! $? -eq 0 ]; then
-    echo "Unable to pull ${DOCKER_IMAGE} image. Are you logged in ${DOCKER_REGISTRY}?"
-    exit 1
-fi
-
 if [ ! -f $(pwd)/openapi.yaml ]; then
     echo "Skipping OpenAPI validation."
     exit 0
 fi
 
-docker run --rm \
-	-v $(pwd)/openapi.yaml:/app/openapi.yaml \
-	${DOCKER_REGISTRY}/${DOCKER_IMAGE} \
-	sh -c \
-        "  openapi bundle --dereferenced /app/openapi.yaml > /app/openapi.der.yaml \
-	        && swagger-cli validate /app/openapi.yaml \
-	        && swagger-cli validate /app/openapi.der.yaml \
-	        && openapi lint /app/openapi.yaml /app/openapi.der.yaml \
-	    "
+if ! command -v "npm" &> /dev/null
+then
+    echo "Unable to find npm. Is it installed and added to your \$PATH?"
+    exit 1
+fi
+
+npm install -g @apidevtools/swagger-cli @redocly/openapi-cli
+
+openapi bundle --dereferenced openapi.yaml > openapi.der.yaml
+
+swagger-cli validate openapi.yaml
+
+swagger-cli validate openapi.der.yaml
+
+openapi lint openapi.yaml openapi.der.yaml
