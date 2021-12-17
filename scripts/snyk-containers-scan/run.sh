c5e4d381ba5d65d3894dd192d32448421ff12d73
Steve Heslouin <steve.heslouin@typeform.com>
2021-12-17 17:25:40 +0100

    adding docker snyk image

--- scripts/snyk-containers-scan/run.sh
+++ scripts/snyk-containers-scan/run.sh
@@ -11,11 +11,13 @@ then
 fi
 
 file_to_search=Dockerfile
+severity_threshold=critical
 repo_dir=$GITHUB_WORKSPACE
+docker_workspace=/opt/workspace/
 repo_name="$(basename "$repo_dir")"
 timestamp=$(date +%s)
-echo $repo_dir
-echo $repo_name
+# echo $repo_dir
+# echo $repo_name
 
 if [ ! -f "$repo_dir/$file_to_search" ]; then
     echo "This repo appear to not contain any Dockerfile, skipping container security scans"
@@ -23,12 +25,17 @@ if [ ! -f "$repo_dir/$file_to_search" ]; then
 fi
 
 #building docker image
-# cd $repo_dir
-# docker build -t $repo_name:$timestamp .
-echo $SNYKTOKEN
+cd $repo_dir
+docker build -t $repo_name:$timestamp .
+docker run --rm --name=snyk_scanner \
+    -t \
+    -e SNYK_TOKEN=${SNYKTOKEN} \
+    -v "${repo_dir}:${docker_workspace}" \
+    --entrypoint=snyk \
+    snyk:latest \
+    test \
+    --docker ${repo_name}:${timestamp} \
+    --file=${docker_workspace}/${file_to_search} \
+    --severity-threshold=${severity_threshold}
 
-
-
-docker images
-# TO DO
 exit 0
