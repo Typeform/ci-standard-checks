cc63e6be268bb3af3146e4b314a0bea710a03d44
Steve Heslouin <steve.heslouin@typeform.com>
2021-12-20 21:46:41 +0100

    test-001

--- scripts/snyk-containers-scan/run.sh
+++ scripts/snyk-containers-scan/run.sh
@@ -25,32 +25,31 @@ if [ ! -f "$repo_dir/$file_to_search" ]; then
 fi
 
 #building docker image
-# cd $repo_dir
-# docker build -t $repo_name:$timestamp .
-# docker run --rm --name=snyk_scanner \
-# 	-t \
-# 	-e SNYK_TOKEN=${SNYKTOKEN} \
-# 	-v "${repo_dir}:${docker_workspace}" \
-# 	--entrypoint=snyk \
-# 	snyk:latest \
-# 	test \
-# 	--docker ${repo_name}:${timestamp} \
-# 	--file=${docker_workspace}/${file_to_search} \
-# 	--severity-threshold=${severity_threshold}
+cd $repo_dir
+docker build -t $repo_name:$timestamp .
+docker run --rm --name=snyk_scanner \
+	-t \
+	-e SNYK_TOKEN=${SNYKTOKEN} \
+	-v "${repo_dir}:${docker_workspace}" \
+	--entrypoint=snyk \
+	567716553783.dkr.ecr.us-east-1.amazonaws.com/security-dummy-repo:1603871124 \
+	test \
+	--docker ${repo_name}:${timestamp} \
+	--file=${docker_workspace}/${file_to_search} \
+	--severity-threshold=${severity_threshold}
 
-# docker pull 567716553783.dkr.ecr.us-east-1.amazonaws.com/security-dummy-repo:1578715105
-# exit_code=$?
-#
-# if [ $exit_code -eq 0 ]; then
-#     echo "Vulnerabilities scan finished. No ${severity_threshold} vulnerabilities were found"
-# elif [ $exit_code -eq 1 ]; then
-#     echo -e "Scan finished. Some ${severity_threshold} vulnerabilities were found, please fix it"
-# elif [ $exit_code -eq 2 ]; then
-#     echo -e "We got a situation here, snyk program failed to complete his task"
-# elif [ $exit_code -eq 3 ]; then
-#     echo -e "Well, that should not happen!!"
-# else
-#     echo "Error scanning"
-# fi
-#
+exit_code=$?
+
+if [ $exit_code -eq 0 ]; then
+    echo "Vulnerabilities scan finished. No ${severity_threshold} vulnerabilities were found"
+elif [ $exit_code -eq 1 ]; then
+    echo -e "Scan finished. Some ${severity_threshold} vulnerabilities were found, please fix it"
+elif [ $exit_code -eq 2 ]; then
+    echo -e "We got a situation here, snyk program failed to complete his task"
+elif [ $exit_code -eq 3 ]; then
+    echo -e "Well, that should not happen!!"
+else
+    echo "Error scanning"
+fi
+
 exit 0
