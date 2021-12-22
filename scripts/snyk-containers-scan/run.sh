460d5bd7342a45870a911dcaefc896d2b502bc17
Steve Heslouin <steve.heslouin@typeform.com>
2021-12-22 09:45:49 +0100

    adjustments

--- scripts/snyk-containers-scan/run.sh
+++ scripts/snyk-containers-scan/run.sh
@@ -41,10 +41,12 @@ docker run --rm --name=snyk_scanner \
     > $stdout_file 2>&1
 
 exit_code=$?
-# tail -n +2 $stdout_file > $stdout_file
+
+# tweak to go around bizzare formatting of github actions web console
 sed -i 1d $stdout_file
+sed -i '/Pro tip/d' $stdout_file
+sed -i '/To remove/d' $stdout_file
 cat $stdout_file
-# grep -v 'Analyzing' $stdout_file
 
 if [ $exit_code -eq 0 ]; then
     echo "Vulnerabilities scan finished. No ${severity_threshold} vulnerabilities were found"
