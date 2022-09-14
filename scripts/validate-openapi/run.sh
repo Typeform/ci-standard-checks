80674a2e98ec802e2a0ebeaf0e252905462324fa
Jordi Roig <jordi.roig@typeform.com>
2022-09-14 16:09:23 +0200

    fix: create config file

--- scripts/validate-openapi/run.sh
+++ scripts/validate-openapi/run.sh
@@ -32,10 +32,20 @@ then
     exit 1
 fi
 
+# Create redocly config file
+cat > .redocly.yaml << EOF
+extends:
+  - recommended
+rules:
+  operation-4xx-response: off
+  security-defined: warn
+EOF
+
+echo "#cli bundle"
 npx @redocly/cli bundle --dereferenced openapi.yaml > openapi.der.yaml
-
+echo "#swagger validate openapi"
 npx @apidevtools/swagger-cli validate openapi.yaml
-
+echo "#swagger validate openapi.der"
 npx @apidevtools/swagger-cli validate openapi.der.yaml
-
+echo "#cli lint"
 npx @redocly/cli lint openapi.yaml openapi.der.yaml
