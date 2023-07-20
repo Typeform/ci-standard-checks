41bdfd81d4a7c5b5adca3cba69532a176e7fca0f
kb-typeform <kevin.barz@typeform.com>
2023-07-20 12:29:59 +0200

    feat(ARCH-372): new dist bundle

--- src/checks/utils.ts
+++ src/checks/utils.ts
@@ -1,9 +1,9 @@
-import * as ts from 'typescript'
+import { getParsedCommandLineOfConfigFile, sys } from 'typescript'
 
 import { TsConfig } from './requiredTypeScript'
 
 export function getTypescriptConfigOptions(filename: string): TsConfig {
   //@ts-expect-error Argument of type 'System' is not assignable to parameter of type 'ParseConfigFileHost'.
-  const options = ts.getParsedCommandLineOfConfigFile(filename, {}, ts.sys)
+  const options = getParsedCommandLineOfConfigFile(filename, {}, sys)
   return { compilerOptions: options?.options }
 }
