4e34a4d5ffb458ab4eff51de65fd8adf29526246
xavi <xespona@gmail.com>
2023-09-15 17:21:36 +0200

    fix(openapi): replace swagger-cli with redocly

--- scripts/validate-openapi/run.sh
+++ scripts/validate-openapi/run.sh
@@ -45,11 +45,7 @@ EOF
 IFS=$'\n' files=($(find . -maxdepth 3 -name openapi.yaml))
 
 for f in ${files[@]}; do
-    npx @apidevtools/swagger-cli validate "$f"
-done
-
-for f in ${files[@]}; do
-    npx @redocly/cli lint "$f"
+    npx @redocly/cli lint --extends=minimal "$f"
 done
 
 npx @redocly/cli bundle --dereferenced --ext json --output openapi.json $(echo ${files})
